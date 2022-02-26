7dcc8b39dfb91b52821073f09c4c4e4eebbb92e7
Dakshi Goel <91268240+Dksie09@users.noreply.github.com>
2022-02-26 09:50:37 +0530

    editted namePage

--- lib/name.dart
+++ lib/name.dart
@@ -1,78 +1,94 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class MyApp extends StatelessWidget {
+class name extends StatelessWidget {
   //const ({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple[200],
+          title: Text(
+            'INFO',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'LexendDeca',
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        backgroundColor: Colors.white,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                // backgroundColor: Colors.orangeAccent,
-                backgroundImage: AssetImage('images/avatar.png'),
-              ),
-              Text(
-                'dakshi goel',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 20,
-                    color: Colors.teal[100]),
-              ),
-              SizedBox(
-                height: 20,
-                width: 150,
-                child: Divider(
-                  thickness: 0.8,
-                  color: Colors.white,
+          child: Card(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 80,
                 ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+919318496977',
-                    style: TextStyle(
-                      fontFamily: 'SourceSansPro',
+                Text(
+                  "Hello, What's your name?",
+                  style: TextStyle(
+                      color: Colors.black,
                       fontSize: 20,
-                      color: Colors.teal,
-                    ),
-                  ),
+                      fontFamily: 'LexendDeca'),
                 ),
-              ),
-              Card(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.teal,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(50),
+                          ),
+                          color: Colors.white),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: TextField(
+                          style: TextStyle(
+                              fontSize: 18, color: Colors.deepPurple[800]),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.deepPurple, width: 0.7),
+                            ),
+                            hintText: "Peter Parker",
+                            hintStyle: TextStyle(
+                                fontSize: 20.0, color: Colors.deepPurple[100]),
+                          ),
+                        ),
+                      ),
                     ),
-                    title: Text(
-                      'dakshiegoel@gmail.com',
-                      style: TextStyle(
-                          color: Colors.teal,
-                          fontSize: 20,
-                          fontFamily: 'SourceSansPro'),
+                    Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: FloatingActionButton(
+                        onPressed: () {},
+                        child: Text('enter'), //Icon(Icons.arrow_forward_ios),
+                        backgroundColor: Colors.purple[400],
+                      ),
                     ),
-                  ))
-            ],
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                  child: Container(
+                      //height: 120,
+                      //width: 150,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('images/back.png'))),
+                      child:
+                          Image(image: AssetImage('images/teesra_phone.png'))),
+                )
+              ],
+            ),
           ),
         ),
       ),
